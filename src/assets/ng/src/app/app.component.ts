e966c9d12d70d896f58fc34b60d92f454a92f461
Michelle <michelle@xmdevelopments.com>
2018-04-09 14:02:14 +1200

    clean up

--- src/assets/ng/src/app/app.component.ts
+++ src/assets/ng/src/app/app.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: './app.component.html'
 })
 export class AppComponent  { 
-    name = 'Worlds'; 
+    text: string = 'Sample code';
+    preview: boolean = false;
+
+    previewContent(e) {
+        e.preventDefault();
+        this.preview = true;
+    }
 }
