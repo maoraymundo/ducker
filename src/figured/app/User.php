2412ffd352e4ce362fe045a9cec6ea9d97489d1e
Michelle Raymundo <michelle.raymundo@snowballeffect.co.nz>
2018-04-14 08:09:24 +1200

    initial commit

--- src/figured/app/User.php
+++ src/figured/app/User.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Models\User as Authenticatable;
 
 class User extends Authenticatable
 {
